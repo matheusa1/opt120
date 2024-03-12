ae70147dccddc6aa649883f2fe8e0591fc7eb76d
Matheus Andrade <2002matheus@gmail.com>
2024-03-11 22:00:01 -0300

    Create createTables.sql

---
+++ createTables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS usuario (
+	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS atividade (
+	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
+    data DATETIME NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS usuario_atividade (
+	usuario_id INTEGER NOT NULL PRIMARY KEY,
+    atividade_id INTEGER NOT NULL,
+    entrega DATETIME NOT NULL,
+    nota FLOAT NOT NULL,
+    
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY (atividade_id) REFERENCES atividade(id)
+);
